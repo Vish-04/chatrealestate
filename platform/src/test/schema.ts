fd6a26be10a706441a5c3fcbd8f08b0b2d776162
nitink23 <50839741+nitink23@users.noreply.github.com>
2024-07-12 12:19:42 -0700

    half of the shema done

--- platform/src/test/schema.ts
+++ platform/src/test/schema.ts
@@ -21,7 +21,38 @@
 // Listings
 // active: boolean
 // listingUrl: string
-
+// Zipcode: string
+// property_description:
+// Year_built:
+// property_images:
+// Property Condition:
+// listing_detail_price:  
+// Features: {
+//       Dining Room Features:
+//       Patio And Porch Features:
+//       Living Room Features:
+//       Window Features:
+//       Bath Features:
+//       Parking Features:
+//       Master Bathroom Features:
+//       Master Bedroom Features:
+//       Cooling:
+//       Heating:
+//       Laundry Features:
+//       Lot Features:
+//       Interior Features:
+//       Kitchen Features:
+//       Association Features:
+//       
+// }
+// mls_number:
+// Bathrooms:
+// Half Bathrooms:
+// Full Bathrooms:
+// Square Footage:
+// # of Rooms
+// Area/District:
+// listing_detail_monthly_cost:
 
 // Relation - Listings leads
 // - user_id : string
